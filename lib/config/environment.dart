0dcf893a57b3db342f5c6e0ae755ce7edf4281c0
raka fajar <rakafajar18@gmail.com>
2021-02-13 21:40:39 +0700

    Sign Up Service API

---
+++ lib/config/environment.dart
@@ -0,0 +1,1 @@
+String baseUrl = 'http://foodmarket-backend.buildwithangga.id/api/';
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -9,39 +9,61 @@ class User extends Equatable {
   final String phoneNumber;
   final String city;
   final String picturePath;
+  static String token;
 
-  User({
-    this.id,
-    this.name,
-    this.email,
-    this.address,
-    this.houseNumber,
-    this.phoneNumber,
-    this.city,
-    this.picturePath,
-  });
+  User(
+      {this.id,
+      this.name,
+      this.email,
+      this.address,
+      this.houseNumber,
+      this.phoneNumber,
+      this.city,
+      this.picturePath});
+
+  factory User.fromJson(Map<String, dynamic> data) => User(
+        id: data['id'],
+        name: data['name'],
+        email: data['email'],
+        address: data['address'],
+        houseNumber: data['houseNumber'],
+        phoneNumber: data['phoneNumber'],
+        city: data['city'],
+        picturePath: data['profile_photo_url'],
+      );
+
+  User copyWith({
+    int id,
+    String name,
+    String email,
+    String address,
+    String houseNumber,
+    String phoneNumber,
+    String city,
+    String picturePath,
+  }) =>
+      User(
+          id: id ?? this.id,
+          name: name ?? this.name,
+          email: email ?? this.email,
+          address: address ?? this.address,
+          houseNumber: houseNumber ?? this.houseNumber,
+          phoneNumber: phoneNumber ?? this.phoneNumber,
+          city: city ?? this.city,
+          picturePath: picturePath ?? this.picturePath);
 
   @override
-  List<Object> get props => [
-        id,
-        name,
-        email,
-        address,
-        houseNumber,
-        phoneNumber,
-        city,
-        picturePath,
-      ];
+  List<Object> get props =>
+      [id, name, email, address, houseNumber, phoneNumber, city, picturePath];
 }
 
 User mockUser = User(
-  id: 1,
-  name: 'Amelia Andini',
-  address: 'Pasir Jati B.131',
-  city: 'Bandung',
-  houseNumber: '1324',
-  phoneNumber: '0888222231231',
-  email: 'raka@raka.com',
-  picturePath:
-      'https://cdn.idntimes.com/content-images/community/2020/11/14-b1fe04b0ceee0bcd7cd47663ff17b2e9.jpg',
-);
+    id: 1,
+    name: 'Jennie Kim',
+    address: 'Jalan Jenderal Sudirman',
+    city: 'Bandung',
+    houseNumber: '1234',
+    phoneNumber: '08123456789',
+    email: 'jennie.kim@blackpink.com',
+    picturePath:
+        'https://i.pinimg.com/474x/8a/f4/7e/8af47e18b14b741f6be2ae499d23fcbe.jpg');
